cf9bc58c164d8294ecf39ace06abdca55b51cb71
CritickalGames <zgtale1@hotmail.com>
2022-01-24 00:36:56 -0300

    carpeta /Indice a /docs

--- docs/dirNAV/dirArquetipos/elementosPHP.php
+++
@@ -1,7 +0,0 @@
-<div class="body" >
-		<h2 align="center">Elementos</h2>
-	<p class="li">
-		Tus poderes iran orientado a todo tipo poder que se considere elemental en cualquier ficción.
-	</p>
-
-</div>
--- docs/dirNAV/dirArquetipos/forcePHP.php
+++
@@ -1,7 +0,0 @@
-<div class="body" >
-		<h2 align="center">Foce</h2>
-	<p class="li">
-		Tus poderes iran orientado a todo tipo de poder físico.
-	</p>
-
-</div>
